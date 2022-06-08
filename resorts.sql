0f1bf497a89878404a8b07ff7b72f2836af06cbb
scottbooma <scottbooma@gmail.com>
2022-06-07 20:11:04 -0400

    Create db and api for resorts

---
+++ resorts.sql
@@ -0,0 +1,9 @@
+TRUNCATE resort;
+
+INSERT INTO resort (id, name, location, image_url) VALUES (
+    1, 'Cataloochee Ski Area', 'Maggie Valley, NC', 'cataloochee-logo.png'
+),(
+    2, 'Beech Mountain Resort', 'Beech Mountain, NC', 'beech-logo.png'
+),(
+    3, 'Sugar Mountain Resort', 'Banner Elk', 'sugar-logo.png'
+);
