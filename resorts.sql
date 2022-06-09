7eb6540f990b5373fd0895536e79044f1419050e
scottbooma <scottbooma@gmail.com>
2022-06-09 15:40:38 -0400

    Modify IDs to line up with external API IDs

--- resorts.sql
+++ resorts.sql
@@ -1,9 +1,9 @@
 TRUNCATE resort;
 
 INSERT INTO resort (id, name, location, image_url, label) VALUES (
-    1, 'Cataloochee Ski Area', 'Maggie Valley, NC', 'cataloochee-logo.png', 'cataloochee'
+    288, 'Cataloochee Ski Area', 'Maggie Valley, NC', 'cataloochee-logo.png', 'cataloochee'
 ),(
-    2, 'Beech Mountain Resort', 'Beech Mountain, NC', 'beech-logo.png', 'beech'
+    287, 'Beech Mountain Resort', 'Beech Mountain, NC', 'beech-logo.png', 'beech'
 ),(
-    3, 'Sugar Mountain Resort', 'Banner Elk, NC', 'sugar-logo.png', 'sugar'
+    290, 'Sugar Mountain Resort', 'Banner Elk, NC', 'sugar-logo.png', 'sugar'
 );
