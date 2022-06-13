ddbd13435252195a532c9f2a60fb1ccbf761b967
scottbooma <scottbooma@gmail.com>
2022-06-13 17:26:10 -0400

    Seed test data into new table

---
+++ reports.sql
@@ -0,0 +1,38 @@
+TRUNCATE reports;
+
+INSERT INTO reports (
+    id,
+    name,
+    date,
+    vibe,
+    description,
+    label
+) VALUES (
+    1,
+    'Doug',
+    '02/18/2022',
+    3,
+    'Slope conditions were pretty good overall. Got there for first tracks and the corduroy was great. It got very busy around lunch time, I guess the holiday crowds are showing up early. Was pretty tracked out by 11am. Left around noon because it was a 15+ minute wait for the lifts.',
+    'cataloochee'
+),(
+    2,
+    'skibro',
+    '02/16/2022',
+    7,
+    'It was pretty icy all day but there was no one here! I got like 20 runs in by lunch. No lines for beers either!',
+    'cataloochee'
+),(
+    3,
+    'John P',
+    '02/15/2022',
+    5,
+    'Icy but fun night skiing.',
+    'cataloochee'
+),(
+    4,
+    'Shredder Girl',
+    '02/14/2022',
+    10,
+    'BF and I came here for Valentine''s Day! We got the first tracks on Snowbird and were pretty much by ourselves until 10. We chilled and had lunch and some drinks at the bar until it started to die down again at 3. Then we ripped it up all night! Got a little icy towards the end but the night vibes were super chill. Everyone was just out there to have a good time! We will defintely be back.',
+    'cataloochee'
+);
